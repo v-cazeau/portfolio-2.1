362b5c49f972e7ee838674767a14fc69440a8941
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-07-20 04:01:46 -0400

    ADDED hero img and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Hero from "./components/Hero"
+import Hero2 from "./components/Hero2"
 import Portfolio from "./components/Portfolio"
+import Certifications from "./components/Certifications"
 import Footer from "./components/Footer"
+import Technologies from "./components/Technologies"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Hero /> 
+      <Hero2 />
       <Portfolio />
+      <Technologies tabIndex={0} />
+      <Hero /> 
+      <Certifications />
       <Footer />
     </main>
   )
